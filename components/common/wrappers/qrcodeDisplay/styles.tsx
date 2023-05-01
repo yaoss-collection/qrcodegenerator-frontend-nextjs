eebbb786047dc68f5030f974ca201af13528fde3
Zharkan <g.j@mailbox.org>
2023-05-01 18:34:12 +0200

    add more shapes

--- components/common/wrappers/qrcodeDisplay/styles.tsx
+++ components/common/wrappers/qrcodeDisplay/styles.tsx
@@ -1,3 +1,4 @@
+import { ArrowSvg } from '@/common/svgs/arrowSvg';
 import { QrStyleContext } from '@/context/index';
 import { Disclosure, Transition } from '@headlessui/react';
 import clsx from 'clsx';
@@ -5,7 +6,6 @@ import { useContext } from 'react';
 
 type DetailsProps = {
   title: string;
-  className: string;
 };
 
 type ButtonProps = {
@@ -14,17 +14,17 @@ type ButtonProps = {
   onClick?: () => void;
 };
 
-const Styles = ({ title, className }: DetailsProps) => {
+const Styles = ({ title }: DetailsProps) => {
   const { state, dispatch } = useContext(QrStyleContext);
 
   return (
-    <div className={className}>
+    <>
       <Disclosure>
         {({ open }) => (
           <>
             <Disclosure.Button
               className={clsx(
-                'flex w-full justify-between rounded-lg bg-secondary-light py-2 py-5 pl-6 text-left text-sm text-white focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-opacity-75',
+                'mt-10 flex w-full items-center justify-between rounded-lg bg-secondary-light py-5 pl-6 text-left text-sm text-white focus:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-opacity-75',
                 {
                   'rounded-b-lg': !open,
                   'rounded-b-none': open,
@@ -32,6 +32,14 @@ const Styles = ({ title, className }: DetailsProps) => {
               )}
             >
               <span className={'font-semibold uppercase'}>{title}</span>
+              <span
+                className={clsx('mr-5', {
+                  'rotate-180 transform': !open,
+                  'rotate-0 transform': open,
+                })}
+              >
+                <ArrowSvg />
+              </span>
             </Disclosure.Button>
             <>
               <Transition
@@ -41,39 +49,96 @@ const Styles = ({ title, className }: DetailsProps) => {
                 leave={'transition duration-75 ease-out'}
                 leaveFrom={'transform translate-y-0 opacity-100'}
                 leaveTo={'transform -translate-y-1/2 opacity-0'}
+                unmount={false}
               >
                 <Disclosure.Panel
-                  className={
-                    'flex justify-center rounded-b-lg bg-secondary-light px-4 pb-8 pt-2 text-sm text-gray-500'
-                  }
+                  className={'rounded-b-lg bg-secondary-light px-6 pb-8 pt-2 text-sm text-gray-500'}
                 >
-                  <Button
-                    title={'Squares'}
-                    active={state.style === 'square'}
-                    onClick={() =>
-                      dispatch({
-                        type: 'SET_QR_STYLE',
-                        payload: { style: 'square' },
-                      })
-                    }
-                  />
-                  <Button
-                    title={'Dots'}
-                    active={state.style === 'dot'}
-                    onClick={() =>
-                      dispatch({
-                        type: 'SET_QR_STYLE',
-                        payload: { style: 'dot' },
-                      })
-                    }
-                  />
+                  <div className={'flex flex-wrap justify-center'}>
+                    <Button
+                      title={'Squares'}
+                      active={state.style === 'square'}
+                      onClick={() =>
+                        dispatch({
+                          type: 'SET_QR_STYLE',
+                          payload: { style: 'square', dotType: 'square' },
+                        })
+                      }
+                    />
+                    <Button
+                      title={'Dots'}
+                      active={state.style === 'dot'}
+                      onClick={() =>
+                        dispatch({
+                          type: 'SET_QR_STYLE',
+                          payload: { style: 'dot' },
+                        })
+                      }
+                    />
+                  </div>
+                  {state.style === 'dot' && (
+                    <Transition
+                      enter={'transition duration-100 ease-out'}
+                      enterFrom={'transform -translate-y-1/2 opacity-0'}
+                      enterTo={'transform translate-y-0 opacity-100'}
+                      leave={'transition duration-75 ease-out'}
+                      leaveFrom={'transform translate-y-0 opacity-100'}
+                      leaveTo={'transform -translate-y-1/2 opacity-0'}
+                      show={state.style === 'dot'}
+                    >
+                      <div className={'mt-5 flex flex-col justify-center px-1'}>
+                        <p className={'mb-3'}>
+                          <span className={'text-xs font-semibold uppercase text-white'}>
+                            Dot Type customizations
+                          </span>
+                        </p>
+                        <div className={'flex flex-wrap gap-y-3'}>
+                          <Button
+                            title={'Rounded*'}
+                            active={state.dotType === 'rounded'}
+                            onClick={() =>
+                              dispatch({
+                                type: 'SET_QR_DOT_TYPE',
+                                payload: { dotType: 'rounded' },
+                              })
+                            }
+                          />
+                          <Button
+                            title={'Classy*'}
+                            active={state.dotType === 'classy'}
+                            onClick={() =>
+                              dispatch({
+                                type: 'SET_QR_DOT_TYPE',
+                                payload: { dotType: 'classy' },
+                              })
+                            }
+                          />
+                          <Button
+                            title={'Square'}
+                            active={state.dotType === 'square'}
+                            onClick={() =>
+                              dispatch({
+                                type: 'SET_QR_DOT_TYPE',
+                                payload: { dotType: 'square' },
+                              })
+                            }
+                          />
+                        </div>
+                        <p className={'mt-5 text-xs font-light text-white'}>
+                          * Dot types <strong>&quot;Rounded&quot;</strong> and{' '}
+                          <strong>&quot;Classy&quot;</strong> can alter the QR code&apos;s
+                          readability.
+                        </p>
+                      </div>
+                    </Transition>
+                  )}
                 </Disclosure.Panel>
               </Transition>
             </>
           </>
         )}
       </Disclosure>
-    </div>
+    </>
   );
 };
 
--- context/index.tsx
+++ context/index.tsx
@@ -1,21 +1,23 @@
-import { CornerSquareType } from 'qr-code-styling';
+import { CornerSquareType, DotType } from 'qr-code-styling';
 import React, { createContext, useMemo, useReducer } from 'react';
 import { IProps } from 'react-qrcode-logo';
 
 const initialState: IState = {
   style: 'square',
+  dotType: 'square',
   value: "I'm EMPTY",
 };
 
 interface IState {
   style?: CornerSquareType;
+  dotType?: DotType;
   value: IProps['value'];
 }
 
-type Actions = 'SET_QR_STYLE' | 'SET_QR_VALUE';
+type Actions = 'SET_QR_STYLE' | 'SET_QR_VALUE' | 'SET_QR_DOT_TYPE';
 interface IAction {
   type: Actions;
-  payload: { style?: CornerSquareType; value?: IProps['value'] };
+  payload: { style?: CornerSquareType; value?: IProps['value']; dotType?: DotType };
 }
 
 interface IContextProps {
@@ -36,11 +38,17 @@ const reducer = (state: IState, action: IAction) => {
       return {
         ...state,
         style: action.payload.style,
+        dotType: action.payload.dotType,
       };
     case 'SET_QR_VALUE':
       return {
         ...state,
         value: action.payload.value,
+      };
+    case 'SET_QR_DOT_TYPE':
+      return {
+        ...state,
+        dotType: action.payload.dotType,
       };
     default:
       return state;
